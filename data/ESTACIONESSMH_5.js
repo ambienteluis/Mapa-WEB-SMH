03135f806057a6a2a4bd2fe6dd4fed90b83abcd9
ambienteluis <153576819+ambienteluis@users.noreply.github.com>
2025-03-17 11:10:11 -0500

    Add files via upload

---
+++ data/ESTACIONESSMH_5.js
@@ -0,0 +1,13 @@
+var json_ESTACIONESSMH_5 = {
+"type": "FeatureCollection",
+"name": "ESTACIONESSMH_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "X": 766513.392, "Y": 9207225.871, "Estado": "Midiendo", "Ini_regist": "2024-02-25", "Foto": "E:\\Fotos SMH\\Hidro_01.jpeg", "Estación": "Hidrométrica 01", "Contribuy.": "Contribuyentes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.586879006104766, -7.16576547047266 ] ] } },
+{ "type": "Feature", "properties": { "X": 767340.279, "Y": 9206773.788000001, "Estado": "Midiendo", "Ini_regist": "2024-02-29", "Foto": "E:\\Fotos SMH\\Hidro_02.jpg", "Estación": "Hidrométrica 02", "Contribuy.": "Contribuyentes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.57937500601345, -7.169811946676743 ] ] } },
+{ "type": "Feature", "properties": { "X": 766239.293, "Y": 9207158.976, "Estado": "Midiendo", "Ini_regist": "2024-02-24", "Foto": "E:\\Fotos SMH\\Meteo_01.jpg", "Estación": "Meteorológica 01", "Contribuy.": "Juan Caja Garcia" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.58935613699083, -7.166383070447945 ] ] } },
+{ "type": "Feature", "properties": { "X": 764509.145, "Y": 9208277.144, "Estado": "Midiendo", "Ini_regist": "2024-11-15", "Foto": "E:\\Fotos SMH\\Meteo_02.jpg", "Estación": "Meteorológica 02", "Contribuy.": "Terreno Comunal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.605065036547103, -7.156358985639791 ] ] } },
+{ "type": "Feature", "properties": { "X": 765585.595, "Y": 9209872.254000001, "Estado": "Midiendo", "Ini_regist": "2024-12-05", "Foto": "E:\\Fotos SMH\\Esta_03.jpg", "Estación": "Meteorológica 03", "Contribuy.": "Alejandro Lopez Castrejon" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.595399847491507, -7.141891760823647 ] ] } },
+{ "type": "Feature", "properties": { "X": 772012.019, "Y": 9208311.166, "Estado": "Midiendo", "Ini_regist": "2024-12-06", "Foto": "E:\\Fotos SMH\\Turbidimetro.jpg", "Estación": "Turbidímetro", "Contribuy.": "EPS SEDACAJ S.A." }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -78.537175869983784, -7.15569319730173 ] ] } }
+]
+}
